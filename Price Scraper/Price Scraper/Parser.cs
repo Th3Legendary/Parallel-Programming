d33cf4a7ef10afa74ca8d4b9ec4926d4cebf724f
Th3Legendary <thelegendarybg@gmail.com>
2021-11-04 23:16:43 +0200

    Add Homework 3

---
+++ Price Scraper/Price Scraper/Parser.cs
@@ -0,0 +1,28 @@
+﻿using Fizzler.Systems.HtmlAgilityPack;
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace PriceScraper
+{
+    class Parser
+    {
+        public static string Parse(string link)
+        {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(GetHTML(link));
+            string productTitle = htmlDoc.DocumentNode.QuerySelector(".product-title h1")?.InnerText.Trim();
+            string mainPrice = htmlDoc.DocumentNode.QuerySelector("#price-tag")?.InnerText.Trim();
+            string coins = htmlDoc.DocumentNode.QuerySelector(".after-decimal")?.InnerText.Trim();
+            string currency = htmlDoc.DocumentNode.QuerySelector(".currency")?.InnerText.Trim();
+            return $"The price for {productTitle} is: {mainPrice}{coins} {currency}";
+        }
+        static string GetHTML(string link)
+        {
+            HttpClient client = new HttpClient();
+            return client.GetStringAsync(link).Result;
+        }
+    }
+}
---
+++ Price Scraper/Price Scraper/Program.cs
@@ -0,0 +1,49 @@
+﻿using Fizzler.Systems.HtmlAgilityPack;
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PriceScraper
+{
+    class Program
+    {
+        static List<string> productLinks = new List<string>
+            {
+                "https://ardes.bg/product/msi-geforce-rtx-3070-8gb-gaming-z-trio-lhr-912-v390-271-213251",
+                "https://ardes.bg/product/apple-macbook-pro-13-2020-z11c0000g-204416",
+                "https://ardes.bg/product/27-lg-27gn750-b-27gn750-b-175856",
+                "https://ardes.bg/product/48-lg-oled48c11lb-oled48c11lb-207797"
+            };
+        static async Task Main(string[] args)
+        {          
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            string[] result = await BeginAsync();
+            stopwatch.Stop();
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            PrintResult(result);
+            Console.WriteLine("Elapsed time: " + stopwatch.ElapsedMilliseconds);
+            Console.WriteLine("----------------------------------");         
+        }
+        static async Task<string[]> BeginAsync()
+        {
+            List<Task<string>> tasks = new List<Task<string>>();
+            foreach (string link in productLinks)
+            {
+                tasks.Add(Task.Run(() => Parser.Parse(link)));
+            }
+            var result = await Task.WhenAll(tasks);
+            return result;
+        }
+        static void PrintResult(string[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
